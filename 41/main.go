22f3e12f22f841ca8f953bd06059871841c59bce
QuatreMains <plutokor@gmail.com>
2020-07-20 16:14:06 +0900

    연습문제 PASS

--- 41/main.go
+++ 41/main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"code.google.com/p/go-tour/wc"
+	"strings"
+)
+
+func WordCount(s string) map[string]int {
+	var m map[string]int = map[string]int {}
+
+	arr := strings.Fields(s)
+	for i := 0; i < len(arr); i++ {
+		m[arr[i]]++
+	}
+	return m
+}
+
 func main() {
+	wc.Test(WordCount)
 }
